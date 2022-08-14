3b777b18a9c9437369c95a7a4c5cfc39d420ce2f
Motaz Hesham <motazhesham01@yahoo.com>
2022-08-14 15:53:23 +0200

    first commit

---
+++ resources/views/admin/receipts/search_phone.blade.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-xs-2">
+        <h5>{{__('Social Receipt')}}</h5>
+        <b> {{ $receipt_social }} </b>
+    </div>
+    <div class="col-xs-2">
+        <h5>{{__('Figures Receipt')}}</h5>
+        <b> {{ $receipt_figures }} </b>
+    </div>
+    <div class="col-xs-2">
+        <h5>{{__('Company Receipts')}}</h5>
+        <b> {{ $receipt_company }} </b>
+    </div>
+    <div class="col-xs-2">
+        <h5>{{__('Clients Receipt')}}</h5>
+        <b> {{ $receipt_client }} </b>
+    </div>
+    <div class="col-xs-2">
+        <h5>{{__('Customers Orders')}}</h5>
+        <b> {{ $customers_orders }} </b>
+    </div>
+    <div class="col-xs-2">
+        <h5>{{__('Sellers Orders')}}</h5>
+        <b> {{ $sellers_orders }} </b>
+    </div>
+</div>
